45342a80a7102d8d9e967fb36bb270d0e1e4e6db
Blazich <rumelhart@protonmail.ch>
2018-02-23 08:17:33 +0100

    Changed the structure, a bit. Added couple more functions.

--- acafiler.sh
+++ acafiler.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-BIBLIOGRAPHY_DIR=/home/blazej/Documents/notatki/bibliografia/bibliography.bibtex
+BIBLIOGRAPHY_FILE=/home/blazej/Documents/notatki/bibliografia/bibliography.bibtex
 THE_FILE="$1"
+json_data=
 
-it_is_pdf () {
+usage () {
+
+}
+
+is_pdf () {
 # Use "file" to check if the file is a PDF.
     file_extension=$(file -b "$THE_FILE")
     if [[ ${file_extension::3} == PDF ]]; then
@@ -13,29 +18,48 @@ it_is_pdf () {
     fi
 }
 
-has_doi_in_meta () {
+#it_is_doi () {
+    #if
+    #fi
+#}
+
+has_doi () {
 # Unelegantly check if it has doi url in meta-data
-# And save it in the bib file
     doi_meta=$(exiftool "$THE_FILE" | sed -n 's/^.*\(10.[0-9]\{4\}.*\)/\1/p'|
                uniq)
     if [[ -n $doi_meta && $doi_meta != $THE_FILE ]]; then #MODYFIKACJA WYMAGANA
         doi_meta=http://dx.doi.org/"$doi_meta"
-        echo "$doi_meta"
+        # For debug uncomment below
+        #echo "$doi_meta"
     else
         echo "The file: '$(basename "$THE_FILE")' has no doi in metadata."
         # TODO
         # Find doi in the file itself using pdftotext
     fi
+    return $doi_meta
+}
+
+get_json_bib () {
+    # Get json data about the article
+    # Clean it from surrounding whitespaces
+    # Add new lines
+    scraped=$(curl -sLH "Accept: text/bibliography; style=bibtex" $doi_meta)
+    cleaned=$(while read REPLY; do echo "$REPLY"; done <<< $scraped)
+    formated= # Use sed, to add newlines and format it
+    echo "Succesfully scraped, cleaned and formated #Title"
+
+    # TODO
+    # If connection unsuccessfull - print error msg >&2
+
 }
 
 main () {
-    if it_is_pdf; then
-        has_doi_in_meta
-# Having doi, it gets bib data from from the internet,then adds to bib file.
-        curl -LH "Accept: text/bibliography; style=bibtex" $doi_meta # >> \
-#        $BIBLIOGRAPHY_DIR 
+    if is_pdf; then
+        has_doi
+        get_json_bib
     fi
 }
+
 main
 
     
